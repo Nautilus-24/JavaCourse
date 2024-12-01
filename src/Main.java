39161540bf71931a93c26f83419637633caf0fa5
Nautilus-24 <viktornautilus@gmail.com>
2024-12-01 13:57:15 +0300

    test1

---
+++ src/Main.java
@@ -0,0 +1,17 @@
+/* *
+   *@author Viktor Nautilus
+   * @version 1.01 2024
+ */
+
+//This is Main class
+public class Main {
+    // Этот метод является точкой входа в программу
+    public static void main(String[] args) {
+        /*
+         Эта инструкция
+          выводит строку на экран
+         */
+        System.out.println("Hello");
+        System.out.println("Привет, иеня зовут Виктор !");
+    }
+}
